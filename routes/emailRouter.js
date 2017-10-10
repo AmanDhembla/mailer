8fc07a71c6b393c8c1961318d3c910f5cab974fc
amandhembla@gmail.com <amandhembla@gmail.com>
2017-10-10 23:20:03 +0530

    changing the hardcoded message with request message

--- routes/emailRouter.js
+++ routes/emailRouter.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.route("/confirmation")
 .post(function(req,res,next){
 	if(req.body.key=process.env.key){
-		sendEmail(req.body.to,req.body.subject,"do as fast as possible");
+		sendEmail(req.body.to,req.body.subject,req.body.message);
 		res.json({success:true});
 	}
 	else{
