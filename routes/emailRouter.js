aae9d6a34686cd391bce6ec2e24790a80aff2977
amandhembla@gmail.com <amandhembla@gmail.com>
2017-10-10 19:09:53 +0530

    validation done before sending mails

--- routes/emailRouter.js
+++ routes/emailRouter.js
@@ -4,8 +4,14 @@ var router = express.Router();
 
 router.route("/confirmation")
 .post(function(req,res,next){
-	sendEmail(req.body.to,req.body.subject,"do as fast as possible");
-	res.json({success:true});
+	if(req.body.key=process.env.key){
+		sendEmail(req.body.to,req.body.subject,"do as fast as possible");
+		res.json({success:true});
+	}
+	else{
+		res.json({success:false});
+	}
+
 });
 
 module.exports=router;
